dae8c10bcacf4d42720c72d0d48278b3c8e89428
Ranbir-GNE <ranbir2115113@gndec.ac.in>
2024-10-01 12:39:14 +0530

    setting up cors again

--- index.js
+++ index.js
@@ -6,38 +6,22 @@ const todoRouter = require("./routes/todoRoutes.cjs");
 const userRouter = require("./routes/userRoutes.cjs");
 const userMiddleware = require("./middleware/userMiddleware.cjs");
 require("dotenv").config();
-const fs = require("fs");
-const { format } = require("date-fns");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 dbConnection();
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
 });
 
-function logger(req, res, next) {
-  const clientIp = req.ip || req.connection.remoteAddress;
-  const accessedUrl = req.originalUrl;
-  const formattedDate = format(new Date(), "dd-mm-yyyy:hh:mm:ss");
-  const method = req.method;
-  const logEntry = `Date: ${formattedDate} - IP: ${clientIp}, URL: ${accessedUrl} Method: ${method}\n`;
-
-  try {
-    fs.appendFile("./logger.txt", logEntry, (err) => {
-      if (err) {
-        console.error("Failed to write log:", err);
-      }
-    });
-    next();
-  } catch (error) {
-    console.error("Failed to write log:", error);
-  }
-}
-
-app.use(logger);
+var corsOptions = {
+  origin: "https://todos-backend-rouge.vercel.app/api/todos",
+  methods: "GET,PUT,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 app.use("/api/todos", userMiddleware, todoRouter);
 app.use("/api/user", userRouter);
